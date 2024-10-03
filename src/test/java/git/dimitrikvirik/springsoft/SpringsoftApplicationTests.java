203d58408950e3185252780e6fadf9aa6e967907
dimitri <d.kvirikashvili@inforttech.com>
2024-10-03 11:17:09 +0400

    initial commit

---
+++ src/test/java/git/dimitrikvirik/springsoft/SpringsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package git.dimitrikvirik.springsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
