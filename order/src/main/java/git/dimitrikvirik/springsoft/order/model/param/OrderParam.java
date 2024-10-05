0a3aa3237b95be7df33dd50ef06ab2683d531476
dimitri <d.kvirikashvili@inforttech.com>
2024-10-05 10:39:43 +0400

    Order Integration Test

--- order/src/main/java/git/dimitrikvirik/springsoft/order/model/param/OrderParam.java
+++ order/src/main/java/git/dimitrikvirik/springsoft/order/model/param/OrderParam.java
@@ -2,9 +2,13 @@ package git.dimitrikvirik.springsoft.order.model.param;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import git.dimitrikvirik.springsoft.order.model.OrderStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
@@ -13,15 +17,19 @@ public class OrderParam {
 
 
     @JsonProperty("product")
+    @NotBlank
     private String product;
 
     @JsonProperty("quantity")
+    @Min(1)
     private Long quantity;
 
     @JsonProperty("price")
+    @Range(min = 0, max = 1000000)
     private Double price;
 
     @JsonProperty("status")
+    @NotNull
     private OrderStatus status;
 
 }
